f337c32a8cec600212aa142ab55abbe208f3353f
Nisal Dissanayake <nisal@avatarin.com>
2023-08-19 21:51:32 +0900

    refactor

--- Walnut/src/Walnut/GraphicsAPI/OpenGLImage.cpp
+++ Walnut/src/Walnut/GraphicsAPI/OpenGLImage.cpp
@@ -14,20 +14,13 @@ void OpenGLImage::CreateImage(VkFormat vulkanFormat, uint32_t width, uint32_t he
     m_Height = height;
     GLenum internalFormat = GL_RGBA8;
     m_DataFormat = GL_RGBA;
-    // glCreateTextures(GL_TEXTURE_2D, 1, &m_texture);
-    // glTextureStorage2D(m_texture, 1, internalFormat, m_Width, m_Height);
 
-    // glTextureParameteri(m_texture, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    // glTextureParameteri(m_texture, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    // glTextureParameteri(m_texture, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    // glTextureParameteri(m_texture, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-    glGenTextures(1, &m_texture);
+    glGenTextures(1, &m_texture); // glCreateTextures(GL_TEXTURE_2D, 1, &m_texture);
+    
     glBindTexture(GL_TEXTURE_2D, m_texture);
 
     // Load up some initial data
-    glTexImage2D(GL_TEXTURE_2D, 0, m_DataFormat, 256, 256, 0, m_DataFormat, GL_UNSIGNED_BYTE, (GLvoid*)m_imageBuffer);
+    glTexImage2D(GL_TEXTURE_2D, 0, m_DataFormat, m_Width, m_Height, 0, m_DataFormat, GL_UNSIGNED_BYTE, (GLvoid*)m_imageBuffer);
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // GL_CLAMP
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT); // GL_CLAMP
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); // Linear Filtering
@@ -40,11 +33,9 @@ void OpenGLImage::CreateImageView(VkFormat vulkanFormat)
 
 void OpenGLImage::UploadToBuffer(const void *data, size_t uploadSize, size_t alignedSize)
 {
-    //glTextureSubImage2D(m_texture, 0, 0, 0, m_Width, m_Height, m_DataFormat, GL_UNSIGNED_BYTE, data);
-
     glBindTexture(GL_TEXTURE_2D, (GLuint)(intptr_t)m_texture);
-		// Load up the new data
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 256, 256, 0, GL_RGBA, GL_UNSIGNED_BYTE, (GLvoid*)data);
+	// Load up the new data
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m_Width, m_Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, (GLvoid*)data);
 }
 
 void OpenGLImage::CreateSampler()
@@ -62,11 +53,12 @@ ImTextureID OpenGLImage::GetDescriptorSet()
 
 bool OpenGLImage::ImageAvailable()
 {
-    return false;
+    return true;
 }
 
 void OpenGLImage::ResourceFree()
 {
+    glDeleteTextures(1, &m_texture);
 }
 
 }
