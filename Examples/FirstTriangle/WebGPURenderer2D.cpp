355ec53a6b26b843bdf111e1a7b2215763a4f685
Nisal Dissanayake <nisal@avatarin.com>
2023-11-04 18:48:20 +0900

    add shader and pipeline

--- Examples/FirstTriangle/WebGPURenderer2D.cpp
+++ Examples/FirstTriangle/WebGPURenderer2D.cpp
@@ -1,5 +1,185 @@
 #include "WebGPURenderer2D.h"
 
+const char* shaderSource = R"(
+@vertex
+fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> @builtin(position) vec4<f32> {
+	var p = vec2f(0.0, 0.0);
+	if (in_vertex_index == 0u) {
+		p = vec2f(-0.5, -0.5);
+	} else if (in_vertex_index == 1u) {
+		p = vec2f(0.5, -0.5);
+	} else {
+		p = vec2f(0.0, 0.5);
+	}
+	return vec4f(p, 0.0, 1.0);
+}
+
+@fragment
+fn fs_main() -> @location(0) vec4f {
+    return vec4f(0.0, 0.4, 1.0, 1.0);
+}
+)";
+
+void GraphicsAPI::WebGPURenderer2D::CreateSwapChain(uint32_t width, uint32_t height)
+{
+    wgpu::SwapChainDescriptor swapChainDesc;
+	swapChainDesc.width = width;
+	swapChainDesc.height = height;
+	swapChainDesc.usage = wgpu::TextureUsage::RenderAttachment;
+	swapChainDesc.format = WebGPU::GetSwapChainFormat();
+	swapChainDesc.presentMode = wgpu::PresentMode::Fifo;
+
+    m_rendererSwapChain = GraphicsAPI::WebGPU::GetDevice().createSwapChain(GraphicsAPI::WebGPU::GetSurface(), swapChainDesc);
+    std::cout << "Swapchain: " << m_rendererSwapChain << std::endl;
+    if (m_rendererSwapChain == nullptr)
+        std::cerr << "Could not initialize Renderer SwapChain!" << std::endl;
+}
+
+void GraphicsAPI::WebGPURenderer2D::CreateShaders()
+{
+    std::cout << "Creating shader module..." << std::endl;
+
+    wgpu::ShaderModuleDescriptor shaderDesc;
+#ifdef WEBGPU_BACKEND_WGPU
+    shaderDesc.hintCount = 0;
+    shaderDesc.hints = nullptr;
+#endif
+
+    wgpu::ShaderModuleWGSLDescriptor shaderCodeDesc;
+    // Set the chained struct's header
+    shaderCodeDesc.chain.next = nullptr;
+    shaderCodeDesc.chain.sType = wgpu::SType::ShaderModuleWGSLDescriptor;
+    shaderCodeDesc.code = shaderSource;
+    // Connect the chain
+    shaderDesc.nextInChain = &shaderCodeDesc.chain;
+    m_shaderModule = WebGPU::GetDevice().createShaderModule(shaderDesc);
+
+    std::cout << "Shader module: " << m_shaderModule << std::endl;
+}
+
+void GraphicsAPI::WebGPURenderer2D::CreatePipeline()
+{
+    std::cout << "Creating render pipeline..." << std::endl;
+
+    wgpu::RenderPipelineDescriptor pipelineDesc;
+
+    // Vertex fetch
+	// (We don't use any input buffer so far)
+    pipelineDesc.vertex.bufferCount = 0;
+    pipelineDesc.vertex.buffers = nullptr;
+
+    // Vertex shader
+    pipelineDesc.vertex.module = m_shaderModule;
+	pipelineDesc.vertex.entryPoint = "vs_main";
+    pipelineDesc.vertex.constantCount = 0;
+	pipelineDesc.vertex.constants = nullptr;
+
+    // Primitive assembly and rasterization
+	// Each sequence of 3 vertices is considered as a triangle
+	pipelineDesc.primitive.topology = wgpu::PrimitiveTopology::TriangleList;
+	// We'll see later how to specify the order in which vertices should be
+	// connected. When not specified, vertices are considered sequentially.
+	pipelineDesc.primitive.stripIndexFormat = wgpu::IndexFormat::Undefined;
+	// The face orientation is defined by assuming that when looking
+	// from the front of the face, its corner vertices are enumerated
+	// in the counter-clockwise (CCW) order.
+	pipelineDesc.primitive.frontFace = wgpu::FrontFace::CCW;
+	// But the face orientation does not matter much because we do not
+	// cull (i.e. "hide") the faces pointing away from us (which is often
+	// used for optimization).
+	pipelineDesc.primitive.cullMode = wgpu::CullMode::None;
+
+    // Fragment shader
+	wgpu::FragmentState fragmentState;
+	pipelineDesc.fragment = &fragmentState;
+	fragmentState.module = m_shaderModule;
+	fragmentState.entryPoint = "fs_main";
+	fragmentState.constantCount = 0;
+	fragmentState.constants = nullptr;
+
+    // Configure blend state
+	wgpu::BlendState blendState;
+	// Usual alpha blending for the color:
+	blendState.color.srcFactor = wgpu::BlendFactor::SrcAlpha;
+	blendState.color.dstFactor = wgpu::BlendFactor::OneMinusSrcAlpha;
+	blendState.color.operation = wgpu::BlendOperation::Add;
+	// We leave the target alpha untouched:
+	blendState.alpha.srcFactor = wgpu::BlendFactor::Zero;
+	blendState.alpha.dstFactor = wgpu::BlendFactor::One;
+	blendState.alpha.operation = wgpu::BlendOperation::Add;
+
+    wgpu::ColorTargetState colorTarget;
+	colorTarget.format = WebGPU::GetSwapChainFormat();
+	colorTarget.blend = &blendState;
+	colorTarget.writeMask = wgpu::ColorWriteMask::All; // We could write to only some of the color channels.
+
+	// We have only one target because our render pass has only one output color
+	// attachment.
+	fragmentState.targetCount = 1;
+	fragmentState.targets = &colorTarget;
+	
+	// Depth and stencil tests are not used here
+	pipelineDesc.depthStencil = nullptr;
+
+    // Multi-sampling
+	// Samples per pixel
+	pipelineDesc.multisample.count = 1;
+	// Default value for the mask, meaning "all bits on"
+	pipelineDesc.multisample.mask = ~0u;
+	// Default value as well (irrelevant for count = 1 anyways)
+	pipelineDesc.multisample.alphaToCoverageEnabled = false;
+
+	// Pipeline layout
+	pipelineDesc.layout = nullptr;
+
+    m_pipeline = WebGPU::GetDevice().createRenderPipeline(pipelineDesc);
+    std::cout << "Render pipeline: " << m_pipeline << std::endl;
+}
+
+void GraphicsAPI::WebGPURenderer2D::Render()
+{
+    wgpu::TextureView nextTexture = m_rendererSwapChain.getCurrentTextureView();
+    if (!nextTexture)
+        std::cerr << "Cannot acquire next swap chain texture" << std::endl;
+
+    wgpu::CommandEncoderDescriptor commandEncoderDesc;
+    commandEncoderDesc.label = "Renderer Command Encoder";
+    wgpu::CommandEncoder encoder = WebGPU::GetDevice().createCommandEncoder(commandEncoderDesc);
+    
+    wgpu::RenderPassDescriptor renderPassDesc;
+
+    wgpu::RenderPassColorAttachment renderPassColorAttachment;
+    renderPassColorAttachment.view = nextTexture;
+    renderPassColorAttachment.resolveTarget = nullptr;
+    renderPassColorAttachment.loadOp = wgpu::LoadOp::Clear;
+    renderPassColorAttachment.storeOp = wgpu::StoreOp::Store;
+    renderPassColorAttachment.clearValue = wgpu::Color{ 0.9, 0.1, 0.2, 1.0 };
+    renderPassDesc.colorAttachmentCount = 1;
+    renderPassDesc.colorAttachments = &renderPassColorAttachment;
+
+    renderPassDesc.depthStencilAttachment = nullptr;
+    renderPassDesc.timestampWriteCount = 0;
+    renderPassDesc.timestampWrites = nullptr;
+    wgpu::RenderPassEncoder renderPass = encoder.beginRenderPass(renderPassDesc);
+
+    // In its overall outline, drawing a triangle is as simple as this:
+    // Select which render pipeline to use
+    renderPass.setPipeline(m_pipeline);
+    // Draw 1 instance of a 3-vertices shape
+    renderPass.draw(3, 1, 0, 0);
+
+    renderPass.end();
+    
+    nextTexture.release();
+
+    wgpu::CommandBufferDescriptor cmdBufferDescriptor;
+    cmdBufferDescriptor.label = "Command buffer";
+    wgpu::CommandBuffer command = encoder.finish(cmdBufferDescriptor);
+    GraphicsAPI::WebGPU::GetQueue().submit(command);
+
+    m_rendererSwapChain.present();
+}
+
 ImTextureID GraphicsAPI::WebGPURenderer2D::GetDescriptorSet()
 {
     return nullptr;
--- Examples/FirstTriangle/WebGPURenderer2D.h
+++ Examples/FirstTriangle/WebGPURenderer2D.h
@@ -15,18 +15,19 @@ namespace GraphicsAPI
         ~WebGPURenderer2D() = default;
 
         size_t CreateUploadBuffer(size_t upload_size);
-        void CreateImage(int vulkanFormat, uint32_t width, uint32_t height);
-        void CreateImageView(int vulkanFormat);
-        void UploadToBuffer(const void* data, size_t uploadSize, size_t alignedSize);
-        void CreateSampler();
+        void CreateSwapChain(uint32_t width, uint32_t height);
+        void CreateShaders();
+        void CreatePipeline();
+        void Render();
         void CreateDescriptorSet();
         ImTextureID GetDescriptorSet();
         bool ImageAvailable();
         void ResourceFree();
         VkBuffer GetStagingBuffer();
     private:
-        wgpu::Texture m_texture = nullptr;
-        wgpu::TextureView m_textureView = nullptr;
+        wgpu::SwapChain m_rendererSwapChain = nullptr;
+        wgpu::ShaderModule m_shaderModule = nullptr;
+        wgpu::RenderPipeline m_pipeline = nullptr;
         uint32_t m_width, m_height;
         uint32_t m_DataFormat;
         void* m_imageBuffer;
