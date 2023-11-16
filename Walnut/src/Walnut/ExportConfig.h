2458aa4c5e3f8f294c6a08068aa660bf97f80fa5
Nisal Dissanayake <nisal@avatarin.com>
2023-11-16 15:22:25 +0900

    add export config file

---
+++ Walnut/src/Walnut/ExportConfig.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#if (defined(_WIN32) || defined(_WIN64))
+
+#ifdef Walnut_EXPORTS
+#define Walnut_API __declspec(dllexport)
+#else
+#define Walnut_API __declspec(dllimport)
+#endif
+#else
+#define Walnut_API
+#endif
