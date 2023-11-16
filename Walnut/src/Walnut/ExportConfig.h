84d260cda7277749bed8e9523132448bf698c3ab
Nisal Dissanayake <nisal@avatarin.com>
2023-11-17 01:12:12 +0900

    make static lib

--- Walnut/src/Walnut/ExportConfig.h
+++ Walnut/src/Walnut/ExportConfig.h
@@ -1,14 +1,14 @@
 #pragma once
 
-#if (defined(_WIN32) || defined(_WIN64))
+// #if (defined(_WIN32) || defined(_WIN64))
 
-#ifdef Walnut_EXPORTS
-#define Walnut_API __declspec(dllexport)
-#else
-#define Walnut_API __declspec(dllimport)
-#endif
-#else
+// #ifdef Walnut_EXPORTS
+// #define Walnut_API __declspec(dllexport)
+// #else
+// #define Walnut_API __declspec(dllimport)
+// #endif
+// #else
+// #define Walnut_API
+// #endif
+
 #define Walnut_API
-#endif
-
-//#define Walnut_API
