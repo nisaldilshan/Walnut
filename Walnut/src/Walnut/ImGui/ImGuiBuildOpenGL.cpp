a248d584aa53215d4f34c32917e9386b71fd6a01
Nisal Dissanayake <nisal@avatarin.com>
2023-11-17 01:36:15 +0900

    fix link errors

--- Walnut/src/Walnut/ImGui/ImGuiBuildOpenGL.cpp
+++ Walnut/src/Walnut/ImGui/ImGuiBuildOpenGL.cpp
@@ -1,4 +1,8 @@
-
+#include <imgui.cpp>
+#include <imgui_demo.cpp>
+#include <imgui_draw.cpp>
+#include <imgui_tables.cpp>
+#include <imgui_widgets.cpp>
 
 #include <imgui_impl_opengl3.cpp>
 #include <imgui_impl_glfw.cpp>
