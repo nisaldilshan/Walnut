93663358b6fa7f81255773ed3652bfeb83ad7558
Nisal Dissanayake <nisal@avatarin.com>
2023-12-17 13:04:49 +0900

    add new geometry load function to load uv coordinates as well

--- Examples/Common/Geometry.h
+++ Examples/Common/Geometry.h
@@ -177,4 +177,66 @@ bool loadGeometryFromObj(const fs::path& path, std::vector<T>& vertexData) {
 	return true;
 }
 
+template<typename T>
+bool loadGeometryFromObjWithUV(const fs::path& path, std::vector<T>& vertexData) {
+	tinyobj::attrib_t attrib;
+	std::vector<tinyobj::shape_t> shapes;
+	std::vector<tinyobj::material_t> materials;
+
+	std::string warn;
+	std::string err;
+
+	// Call the core loading procedure of TinyOBJLoader
+	bool ret = tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, path.string().c_str());
+
+	// Check errors
+	if (!warn.empty()) {
+		std::cout << warn << std::endl;
+	}
+
+	if (!err.empty()) {
+		std::cerr << err << std::endl;
+	}
+
+	if (!ret) {
+		return false;
+	}
+
+	// Filling in vertexData:
+	vertexData.clear();
+	for (const auto& shape : shapes) {
+		size_t offset = vertexData.size();
+		vertexData.resize(offset + shape.mesh.indices.size());
+
+		for (size_t i = 0; i < shape.mesh.indices.size(); ++i) {
+			const tinyobj::index_t& idx = shape.mesh.indices[i];
+
+			vertexData[offset + i].position = {
+				attrib.vertices[3 * idx.vertex_index + 0],
+				-attrib.vertices[3 * idx.vertex_index + 2], // Add a minus to avoid mirroring
+				attrib.vertices[3 * idx.vertex_index + 1]
+			};
+
+			vertexData[offset + i].normal = {
+				attrib.normals[3 * idx.normal_index + 0],
+				-attrib.normals[3 * idx.normal_index + 2],
+				attrib.normals[3 * idx.normal_index + 1]
+			};
+
+			vertexData[offset + i].color = {
+				attrib.colors[3 * idx.vertex_index + 0],
+				attrib.colors[3 * idx.vertex_index + 1],
+				attrib.colors[3 * idx.vertex_index + 2]
+			};
+
+			vertexData[offset + i].uv = {
+				attrib.texcoords[2 * idx.texcoord_index + 0],
+				1 - attrib.texcoords[2 * idx.texcoord_index + 1]
+			};
+		}
+	}
+
+	return true;
+}
+
 }
