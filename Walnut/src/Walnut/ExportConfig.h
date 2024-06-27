c51bba0493d524d7e998a2e029bd46499725b0fb
Nisal Dilshan <bcnisal@gmail.com>
2024-06-28 01:34:48 +0900

    add windows webgpu support

--- Walnut/src/Walnut/ExportConfig.h
+++ Walnut/src/Walnut/ExportConfig.h
@@ -1,14 +1,13 @@
 #pragma once
 
-// #if (defined(_WIN32) || defined(_WIN64))
+#if (defined(_WIN32) || defined(_WIN64))
 
-// #ifdef Walnut_EXPORTS
-// #define Walnut_API __declspec(dllexport)
-// #else
-// #define Walnut_API __declspec(dllimport)
-// #endif
-// #else
-// #define Walnut_API
-// #endif
+#ifdef Walnut_EXPORTS
+#define Walnut_API __declspec(dllexport)
+#else
+#define Walnut_API __declspec(dllimport)
+#endif
 
+#else
 #define Walnut_API
+#endif
