5215af9c70a4efbd96af7ca7d93aa3ee1a47f5c8
Nisal Dilshan <bcnisal@gmail.com>
2026-02-10 00:48:40 +0900

    copy dlls

--- Walnut/src/Walnut/GraphicsAPI/WebGPU/ImGuiBuildWGPU.cpp
+++ Walnut/src/Walnut/GraphicsAPI/WebGPU/ImGuiBuildWGPU.cpp
@@ -1,3 +1,9 @@
+#include <imgui.cpp>
+#include <imgui_demo.cpp>
+#include <imgui_draw.cpp>
+#include <imgui_tables.cpp>
+#include <imgui_widgets.cpp>
+
 #define IMGUI_IMPL_WEBGPU_BACKEND_DAWN
 #include <imgui_impl_wgpu.cpp>
 
