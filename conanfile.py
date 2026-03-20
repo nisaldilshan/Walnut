1093c18bc034f7141e33f96560840bf798576e8a
Nisal Dilshan <bcnisal@gmail.com>
2026-03-21 02:02:09 +0900

    upgrade vulkan loader version

--- conanfile.py
+++ conanfile.py
@@ -66,8 +66,8 @@ class Walnut(ConanFile):
             if self.options.rendering_backend == "OpenGL":
                 self.requires("glad/0.1.33")
             elif self.options.rendering_backend == "Vulkan":
-                self.requires("vulkan-loader/1.3.239.0")
-                self.requires("vulkan-validationlayers/1.3.239.0")
+                self.requires("vulkan-loader/1.3.243.0")
+                self.requires("vulkan-validationlayers/1.3.243.0")
             elif self.options.rendering_backend == "WebGPU":
                 pass
                 #self.requires("WebGPU/latest")
@@ -76,8 +76,8 @@ class Walnut(ConanFile):
             if self.options.rendering_backend == "OpenGL":
                 self.requires("glad/0.1.33")
             elif self.options.rendering_backend == "Vulkan":
-                self.requires("vulkan-loader/1.3.239.0")
-                self.requires("vulkan-validationlayers/1.3.239.0")
+                self.requires("vulkan-loader/1.3.243.0")
+                self.requires("vulkan-validationlayers/1.3.243.0")
             elif self.options.rendering_backend == "WebGPU":
                 pass
                 #self.requires("WebGPU/latest")
@@ -90,12 +90,12 @@ class Walnut(ConanFile):
             if self.options.rendering_backend == "OpenGL":
                 self.requires("glad/0.1.33")
             elif self.options.rendering_backend == "Vulkan":
-                self.requires("vulkan-loader/1.3.239.0")
-                #self.requires("vulkan-validationlayers/1.3.239.0")
+                self.requires("vulkan-loader/1.3.243.0")
+                #self.requires("vulkan-validationlayers/1.3.243.0")
         elif self.settings.os == 'iOS':
             self.options.windowing_system = "SDL"
             if self.options.rendering_backend == "Vulkan":
-                self.requires("vulkan-headers/1.3.239.0")
+                self.requires("vulkan-headers/1.3.243.0")
                 self.requires("moltenvk/1.2.0")
         else:
             raise ConanInvalidConfiguration("Unsupported Platform")
