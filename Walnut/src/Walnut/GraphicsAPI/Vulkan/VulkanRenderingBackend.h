b337ec96cdc31f3f7d614708dc345a2c2a98a051
Nisal Dilshan <bcnisal@gmail.com>
2026-04-01 00:39:02 +0900

    fix linux errors

--- Walnut/src/Walnut/GraphicsAPI/Vulkan/VulkanRenderingBackend.h
+++ Walnut/src/Walnut/GraphicsAPI/Vulkan/VulkanRenderingBackend.h
@@ -39,7 +39,7 @@ public:
 
 private:
 	WalnutWindowHandleType* m_windowHandle = nullptr;
-	std::unique_ptr<GraphicsAPI::VulkanImageRenderPipeline> m_imageRenderPipeline = nullptr;
+	std::unique_ptr<GraphicsAPI::VulkanImageRenderPipeline> m_imageRenderPipeline;
 };
 
 
